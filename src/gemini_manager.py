5a5ec2470ff9d5ddb24d8ba5aa98beb45854a007
Naropap1 <naropap1@gmail.com>
2025-11-23 09:15:54 -0500

    Buffed descriptions

--- src/gemini_manager.py
+++ src/gemini_manager.py
@@ -3,9 +3,11 @@ from google.genai import types
 from src.calendar_manager import CalendarManager
 import datetime
 
-SECRETARY_PREAMBLE = """
+SECRETARY_PROMPT = """
+Current Date: {today}
+
 You are a productivity guru and an expert personal assistant. You are tasked with planning my day today.
-In order to accomplish this task you will be given three things.
+In order to accomplish this task you will be given three key pieces data.
 
 Personal Scheduling Preferences: This is a detailed list of general rules I tend to follow day to day. I am a person with established daily routines.
 For the most part these won't be changed and are going to heavily dictate what windows you have to work with when scheduling!
@@ -32,7 +34,25 @@ Know that there are only so many truly difficult tasks a person can do in a day,
 It is important to have fun in life to keep things fresh.
 Hide fun easter eggs in the task descriptions for me, add fun emojis where you can, and crack jokes when possible.
 If I keep coming back because my calender is entertaining, it would make sure I keep interacting with my productive scheudle.
-Finally you will be updating my calendar directly by using the `add_event` tool to schedule the tasks.
+
+Final pieces of crucial instructions for you to follow.
+You will be updating my calendar directly by using the `add_event` tool to schedule the tasks.
+You will notice that this function has a description parameter. This is where your skills as a productivity guru will shine!
+Add the following information to the description:
+1. What value this event will provide me in life.
+2. If the tasks in the event are large and or daunting, please breakdown the task into actionable steps.
+3. Provide relevant links to the task and or steps when necessary.
+
+Okay you are now ready for the key pieces of data.
+
+Pre-Exisiting Events:
+{existing_events}
+
+Personal Scheduling Preferences:
+{personal_scheduling_preferences}
+
+Potential Tasks:
+{potential_tasks}
 """
 
 
@@ -52,20 +72,12 @@ class GeminiManager:
         today = datetime.date.today()
         existing_events = self.calendar_manager.get_events_for_day(today)
 
-        full_prompt = f"""
-        Current Date: {today}
-
-        {SECRETARY_PREAMBLE}
-
-        Pre-Exisiting Events:
-        {existing_events}
-
-        Personal Scheduling Preferences:
-        {personal_scheduling_preferences}
-
-        Potential Tasks:
-        {potential_tasks}
-        """
+        full_prompt = SECRETARY_PROMPT.format(
+            today=today,
+            existing_events=existing_events,
+            personal_scheduling_preferences=personal_scheduling_preferences,
+            potential_tasks=potential_tasks,
+        )
 
         try:
             chat = self.client.chats.create(
